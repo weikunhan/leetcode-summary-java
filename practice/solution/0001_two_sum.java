dc6cc055290bfa44dc81f6706606f6007ff9ac3e
Weikun Han <weikunhan@g.ucla.edu>
2020-06-08 14:39:19 -0700

    add 0001

---
+++ practice/solution/0001_two_sum.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> valueDict = new HashMap<Integer, Integer>();
+        int[] res = new int[2];
+        
+        for (int i = 0; i < nums.length; i++) {
+            int tempValue = target - nums[i];
+            
+            if (!valueDict.containsKey(nums[i])) {
+                valueDict.put(tempValue, i);
+            } else {
+                res[0] = valueDict.get(nums[i]);
+                res[1] = i;
+                    
+                return res;
+            }
+        }
+        
+        return res;
+    }
+}
