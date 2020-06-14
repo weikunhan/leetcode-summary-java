edc980e5296b1afc47258feeb67b58870aa3a8f5
Weikun Han <weikunhan@g.ucla.edu>
2020-06-13 18:07:51 -0700

    add 696

---
+++ practice/solution/0696_count_binary_substrings.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int countBinarySubstrings(String s) {
+        int left = 0;
+        int right = 1;
+        int res = 0;
+        
+        for (int i = 1; i < s.length(); i++) {
+            if (s.charAt(i) == s.charAt(i - 1)) {
+                right += 1;
+            } else {
+                res += Math.min(left, right);
+                left = right;
+                right = 1;
+            }
+        }
+        
+        res += Math.min(left, right);
+            
+        return res;
+    }
+}
