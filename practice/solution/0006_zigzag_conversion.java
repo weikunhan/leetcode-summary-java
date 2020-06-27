ab9392cca921938381efc42ae2035168546355be
Weikun Han <weikunhan@g.ucla.edu>
2020-06-26 21:50:02 -0700

    add 0006

---
+++ practice/solution/0006_zigzag_conversion.java
@@ -0,0 +1,38 @@
+class Solution {
+    public String convert(String s, int numRows) {
+        StringBuilder[] valueList = new StringBuilder[numRows];
+        int count = 0;
+        int sign = 1;
+        String res = "";
+        
+        if (numRows == 1 || numRows >= s.length()) {
+            res = s;
+            
+            return res;
+        }  
+        
+        for (int i = 0; i < valueList.length; i++) {
+            valueList[i] = new StringBuilder("");   
+        }
+        
+        for (int i = 0; i < s.length(); i++) {
+            valueList[count].append(s.charAt(i));
+            
+            if (count == 0) {
+                sign = 1;
+            }
+            
+            if (count == numRows - 1) {
+                sign = -1;
+            }
+            
+            count += sign;
+        }
+        
+        for (int i = 0; i < valueList.length; i++) {
+            res += valueList[i];
+        }
+        
+        return res;
+    }
+}
