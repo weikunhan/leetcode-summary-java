87702f1a8acb95415c49d854d040e9a9670b4ea0
Weikun Han <weikunhan@g.ucla.edu>
2020-07-20 18:30:54 -0700

    add 0017

---
+++ practice/solution/0017_letter_combinations_of_a_phone_number.java
@@ -0,0 +1,39 @@
+class Solution {
+    public static String[] valueList = { "", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz" };
+    public static List<String> res = new ArrayList<>();
+    
+    public List<String> letterCombinations(String digits) {
+        res = dfs(digits);
+        
+        return res;
+    }
+    
+    public List<String> dfs(String digits) {
+        List<String> tempList = new ArrayList<>();
+        
+        if (digits.length() == 0) {
+            
+            return tempList;
+        }
+        
+        if (digits.length() == 1) {
+            String tempValue = valueList[digits.charAt(0) - '0'];
+            
+            for (int i = 0; i < tempValue.length(); i++) {
+                tempList.add(tempValue.substring(i, i + 1));
+            }
+            
+            return tempList;
+        }
+        
+        String tempValue = valueList[digits.charAt(0) - '0'];
+        
+        for (int i = 0; i < tempValue.length(); i++) {
+            for (String value : dfs(digits.substring(1))) {
+                tempList.add(tempValue.substring(i, i + 1) + value);
+            }
+        }
+                 
+        return tempList;
+    }
+}
